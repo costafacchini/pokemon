934bddb4cb2e57f1e1befac55a195cbb53a48543
Alan Costa Facchini <costafacchini@gmail.com>
2021-02-27 16:10:16 -0300

    Render pokemons from a fixed list

--- src/pokemons.js
+++ src/pokemons.js
@@ -1,6 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const pokemonList = [
+  {
+    "abilities": [
+      "Overgrow", "Fire"
+    ],
+    "detailPageURL": "/us/pokedex/bulbasaur",
+    "weight": 15.2,
+    "weakness": [
+      "Fire",
+      "Psychic",
+      "Flying",
+      "Ice"
+    ],
+    "number": "001",
+    "height": 28.0,
+    "collectibles_slug": "bulbasaur",
+    "featured": "true",
+    "slug": "bulbasaur",
+    "name": "Bulbasaur",
+    "ThumbnailAltText": "Bulbasaur",
+    "ThumbnailImage": "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+    "id": 1,
+    "type": [
+      "grass",
+      "poison"
+    ]
+  },
+  {
+    "abilities": [
+      "Overgrow"
+    ],
+    "detailPageURL": "/us/pokedex/ivysaur",
+    "weight": 28.7,
+    "weakness": [
+      "Fire",
+      "Psychic",
+      "Flying",
+      "Ice"
+    ],
+    "number": "002",
+    "height": 39.0,
+    "collectibles_slug": "ivysaur",
+    "featured": "true",
+    "slug": "ivysaur",
+    "name": "Ivysaur",
+    "ThumbnailAltText": "Ivysaur",
+    "ThumbnailImage": "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png",
+    "id": 2,
+    "type": [
+      "grass",
+      "poison"
+    ]
+  },
+  {
+    "abilities": [
+      "Overgrow"
+    ],
+    "detailPageURL": "/us/pokedex/venusaur",
+    "weight": 9999.0,
+    "weakness": [
+      "Fire",
+      "Psychic",
+      "Flying",
+      "Ice"
+    ],
+    "number": "003",
+    "height": 945.0,
+    "collectibles_slug": "venusaur",
+    "featured": "true",
+    "slug": "venusaur",
+    "name": "Venusaur",
+    "ThumbnailAltText": "Venusaur",
+    "ThumbnailImage": "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png",
+    "id": 3,
+    "type": [
+      "grass",
+      "poison"
+    ]
+  }
+]
 
 function PokemonsIndex() {
+  const [pokemons, setPokemons] = useState(pokemonList);
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -42,7 +125,27 @@ function PokemonsIndex() {
                   <th>Habilidades</th>
                 </tr>
                 </thead>
-                <tbody>                 
+                <tbody>
+                  {pokemons.map((pokemon) => (
+                    <>
+                      <tr key={pokemon["id"]}>
+                        <td>
+                          <img
+                            src={pokemon["ThumbnailImage"]}
+                          />
+                        </td>
+                        <td>{pokemon["ThumbnailAltText"]}</td>
+                        <td>{pokemon["number"]}</td>
+                        <td>
+                          <ul>
+                            {pokemon["abilities"].map(hability => (
+                              <li>{hability}</li>
+                            ))}
+                          </ul>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
                 </tbody>
               </table>
             </div>
